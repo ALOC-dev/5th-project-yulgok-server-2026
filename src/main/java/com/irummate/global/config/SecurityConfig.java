40b02efcef9e395e67d04c70fa8fb6f33a9a1f47
jongparks <roqhdehdqkr@naver.com>
2026-07-13 18:34:52 +0900

    CI/CD Test2

--- src/main/java/com/irummate/global/config/SecurityConfig.java
+++ src/main/java/com/irummate/global/config/SecurityConfig.java
@@ -73,9 +73,7 @@ public class SecurityConfig {
                                 "/swagger-ui.html",
                                 "/v3/api-docs/**",
                                 "/webjars/**",
-                                "/error",                 // 에러 발생 시 리다이렉트되는 경로
-                                "/websocket-chat-test.html",
-                                "/ws/chat/**"
+                                "/error"                 // 에러 발생 시 리다이렉트되는 경로
                         ).permitAll()
 
                         // 그 외의 나머지 모든 요청(anyRequest)은
