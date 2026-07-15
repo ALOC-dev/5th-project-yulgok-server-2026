7e76da43f1b311562db3e2fe714ce38aa6ace296
jongparks <roqhdehdqkr@naver.com>
2026-07-15 20:22:36 +0900

    Certification Update

--- src/main/java/com/irummate/domain/certification/controller/CertificationController.java
+++ src/main/java/com/irummate/domain/certification/controller/CertificationController.java
@@ -47,7 +47,7 @@ public class CertificationController {
         );
     }
 
-    @GetMapping("/presigned-url")
+    @PostMapping("/presigned-url")
     public ResponseEntity<GlobalApiResponse<CertificationPresignResponseDto>> createUploadUrl(
             @AuthenticationPrincipal Long userId,
             @Valid @RequestBody CertificationPresignRequestDto requestDto
